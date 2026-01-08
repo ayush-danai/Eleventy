f75d536efd82b0b38133a39ea42201d047d8fc17
ayushdanai <ayushdanai@gmail.com>
2026-01-08 22:18:34 +0530

    Some nunjucks concepts

--- eleventy_from_scratch/eleventy.config.js
+++ eleventy_from_scratch/eleventy.config.js
@@ -2,3 +2,7 @@ export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("dist");
 }
+export const config = {
+  markdownTemplateEngine: "njk",
+  htmlTemplateEngine: "njk",
+};
